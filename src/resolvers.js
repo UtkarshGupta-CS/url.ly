69d5a08973f37989584c14c8882032fbea3cc430
UtkarshGupta-CS <utgupta1997@gmail.com>
2018-09-20 23:54:28 +0530

    Added getLongUrl resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,31 +1,45 @@
 // Resolvers define the technique for fetching the types in the
 // schema.
+const { promisify } = require("util");
 import { murmurhash3 } from "./utils";
 
 // Resolvers define the technique for fetching the types in the
 // schema.
-const resolvers = client => ({
-  Query: {
-    getUrls: () => [],
-    getLongUrl: (obj, params, context, info) => {
-      return {
-        longUrl: "www.example.com",
-        shortUrl: params.shortUrl
-      };
-    }
-  },
+const resolvers = client => {
+  const getAsync = promisify(client.get).bind(client);
+  return {
+    Query: {
+      getUrls: () => [],
+      getLongUrl: async (obj, params, context, info) => {
+        let longUrl = "";
+        const isKeyExist = await getAsync(`shorturl:${params.shortUrl}`).then(
+          res => {
+            longUrl = res.toString();
+            console.log(longUrl);
+          }
+        );
+        console.log(longUrl);
+        if (isKeyExist) {
+          return {
+            longUrl,
+            shortUrl: params.shortUrl
+          };
+        }
+      }
+    },
 
-  Mutation: {
-    genrateShortUrl: (_, params, context) => {
-      const longUrl = params.longUrl;
-      const shortUrl = murmurhash3(params.longUrl, 1).toString();
-      client.set(`shorturl:${shortUrl}`, longUrl);
-      return {
-        longUrl,
-        shortUrl
-      };
+    Mutation: {
+      genrateShortUrl: (_, params, context) => {
+        const longUrl = params.longUrl;
+        const shortUrl = murmurhash3(params.longUrl, 1).toString();
+        client.set(`shorturl:${shortUrl}`, longUrl);
+        return {
+          longUrl,
+          shortUrl
+        };
+      }
     }
-  }
-});
+  };
+};
 
 export default resolvers;
